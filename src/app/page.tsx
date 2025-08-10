88ed1dcd1e4596498fd055b6feb8b492db311613
TurianoTRW <turianothegreat@gmail.com>
2025-08-10 20:38:07 +0200

    Fix cookies context error by adding return statements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  if (session) redirect("/dashboard");
-  redirect("/auth/sign-in");
+  if (session) return redirect("/dashboard");
+  return redirect("/auth/sign-in");
 }
